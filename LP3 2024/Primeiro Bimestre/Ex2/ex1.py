7165b087122c7b1735d0e6474e75578fd73f8b7c
MeowGamer <126773354+gabi-tarifa@users.noreply.github.com>
2024-04-16 20:41:41 -0300

    Update ex1.py

--- LP3 2024/Primeiro Bimestre/Ex2/ex1.py
+++ LP3 2024/Primeiro Bimestre/Ex2/ex1.py
@@ -2,18 +2,20 @@
 #Informe ao usuário se o palpite está alto ou baixo, até que ele acerte o número.
 import random
 
-numSortido = 1 + (random.choice(range(100)))
-palpite = int(input("Escolha um número de 1 a 100: "))
+def checarNumeros(num, numSortido):
+    if(num == numSortido):
+        return "Ganhaste!!"
+    elif (num < numSortido):
+        return "O número sorteado é maior; escolha outro: "
+    else:
+        return "O número sorteado é menor; escolha outro: "
 
-erro = palpite != numSortido
+numSortido = 1 + (random.choice(range(100)))
+print("Escolha um número de 1 a 100: ")
+
+erro = True
 
 while(erro):
+    palpite = int(input(""))
     erro = palpite != numSortido
-    if(palpite == numSortido):
-        print("Ganhaste!!")
-        break
-    elif (palpite < numSortido):
-        palpite = int(input("O número sorteado é maior; escolha outro: "))
-    else:
-        palpite = int(input("O número sorteado é menor; escolha outro: "))
-    
+    print(checarNumeros(palpite, numSortido))
